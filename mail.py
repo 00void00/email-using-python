884d6550ec9cfc58fdf570b5d96dadb6b2fe17b5
00void00 <63497724+00void00@users.noreply.github.com>
2020-12-04 22:34:14 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,1 +1,15 @@
+>>> from email.message import EmailMessage
+>>> message = EmailMessage()
+>>> sender = "say@gmail.com"
+>>> receiver = "you@gmail.com"
+>>> message['From'] = sender
+>>> message['To'] = receiver
+>>> message['Subject'] = 'Greetings from {} to {}!'.format(sender, receiver)
+>>> body = """Hey there!
+... ...
+... ... I'm learning to send emails using Python!"""
+>>> message.set_content(body)
+>>> print(message)
 
+
+#From, To, and Subject are examples of email header fields
